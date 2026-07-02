15c56664c87ae564c644a9588107545bf85bc33d
IlyasKhan00 <ilyaskhan67777575@gmail.com>
2026-07-02 08:39:27 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+const messages = [
+  "Happy Birthday Mahnoor 💖",
+  "You deserve all the happiness in the world ✨",
+  "Hope your day feels special YAEYY!! 🎂",
+  "You are veryy special JANNUU💕",
+  "Wishing you so much lovee merii jann",
+  "I Love You So Much Mera PETTUU BACHHAWW 💖",
+  "You deserve every good thing in the world",
+  "ILYSMMM :)",
+  "UMWWAAHHHH "
+];
+
+
+function interact() {
+  const msg = messages[Math.floor(Math.random() * messages.length)];
+  document.getElementById("message").innerText = msg;
+}
+
+// 🎊 continuous confetti rain
+function startConfettiRain() {
+  setInterval(() => {
+    createConfetti();
+  }, 120); // speed of falling confetti
+}
+
+function createConfetti() {
+  const c = document.createElement("div");
+  c.classList.add("confetti");
+
+  c.style.left = Math.random() * window.innerWidth + "px";
+  c.style.background = randomColor();
+  c.style.opacity = Math.random();
+
+  c.style.animationDuration = (2 + Math.random() * 3) + "s";
+
+  document.body.appendChild(c);
+
+  setTimeout(() => {
+    c.remove();
+  }, 5000);
+}
+
+function randomColor() {
+  const colors = ["#ff4d6d", "#ffd6e0", "#ffffff", "#ffb6c1", "#a0e7e5"];
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
+// start automatically when page loads
+startConfettiRain();
